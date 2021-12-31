46ba6ce02a457cb5cd3d4dad8d9d68cddb8b707b
kassyJwa <kassy.jwa@nsuslab.com>
2021-12-31 18:27:26 +0900

    SQLite를 사용한 model 데이터 저장 및 html 데이터 연동

--- inflearn_lecture/models.py
+++ inflearn_lecture/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class MyText(models.Model) :
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    contents = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
--- inflearn_lecture/views.py
+++ inflearn_lecture/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from.models import MyText
+
 
 # Create your views here.
 
 def home_list(request) :
-    return render(request, 'inflearn_lecture/home_list.html')
+    texts = MyText.objects.filter()
+    return render(request, 'inflearn_lecture/home_list.html', {'texts': texts})
 
